a4b668e000dd492a785f7aa5cc0cb5bea50085b8
rrd0627 <rrd0627@gmail.com>
2021-09-30 23:56:46 +0900

    leetcode13 fixed

--- codeTest13.cs
+++ codeTest13.cs
@@ -3,41 +3,30 @@ using System.Collections.Generic;
 using System.Text;
 public class Solution
 {
-    Dictionary<string, int> strToValue;
+    Dictionary<char, int> charToValue;
     public int RomanToInt(string s)
     {
         int ret = 0;
-        strToValue = new Dictionary<string, int>();
-        strToValue.Add("M", 1000);
-        strToValue.Add("CM", 900);
-        strToValue.Add("D", 500);
-        strToValue.Add("CD", 400);
-        strToValue.Add("C", 100);
-        strToValue.Add("XC", 90);
-        strToValue.Add("L", 50);
-        strToValue.Add("XL", 40);
-        strToValue.Add("X", 10);
-        strToValue.Add("IX", 9);
-        strToValue.Add("V", 5);
-        strToValue.Add("IV", 4);
-        strToValue.Add("I", 1);
+        int sSize = s.Length;
+        charToValue = new Dictionary<char, int>();
+        charToValue.Add('M', 1000);
+        charToValue.Add('D', 500);
+        charToValue.Add('C', 100);
+        charToValue.Add('L', 50);
+        charToValue.Add('X', 10);
+        charToValue.Add('V', 5);
+        charToValue.Add('I', 1);
 
-        while (s.Length > 0)
+        ret += charToValue[s[sSize - 1]];
+        for (int i = sSize - 2; i >= 0; i--)
         {
-            if(s.Length==1)
+            if (charToValue[s[i]] < charToValue[s[i + 1]])
             {
-                ret += strToValue[s.Substring(0,1)];
-                break;
-            }
-            if(strToValue.TryGetValue(s.Substring(0, 2),out int value))
-            {
-                s=s.Substring(2);
-                ret += value;
+                ret -= charToValue[s[i]];
             }
             else
             {
-                ret += strToValue[s.Substring(0,1)];
-                s=s.Substring(1);
+                ret += charToValue[s[i]];
             }
         }
         return ret;
