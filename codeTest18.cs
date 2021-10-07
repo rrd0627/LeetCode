1a2c64786ab5d6104f54130dc77f05de407f8f19
rrd0627 <rrd0627@gmail.com>
2021-10-07 23:12:47 +0900

    Fixed 18

--- codeTest18.cs
+++ codeTest18.cs
@@ -13,19 +13,15 @@ public class Solution
         Array.Sort(nums);
 
         int left, right;
-        bool isAddI = false,isAddJ = false;
         for (int i = 0; i < nums.Length - 3; i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1] && isAddI) continue;
-            isAddI = false;
-            isAddJ = false;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < nums.Length - 2; j++)
             {
-                if (j > 1 && nums[j] == nums[j - 1] && isAddJ)
+                if (j > i+1 && nums[j] == nums[j - 1])
                 {
                     continue;
                 }
-                isAddJ = false;
                 left = j + 1;
                 right = nums.Length - 1;
 
@@ -43,8 +39,6 @@ public class Solution
                     {
                         ret.Add(new List<int>() { nums[i], nums[j], nums[left], nums[right] });
                         left++;
-                        isAddI = true;
-                        isAddJ = true;
                         while (left < right && nums[left - 1] == nums[left])
                         {
                             left++;
