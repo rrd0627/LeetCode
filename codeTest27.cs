d1cf99960f07c140fbb5f906d4ad75c912f2098c
rrd0627 <rrd0627@gmail.com>
2021-10-22 00:04:44 +0900

    fixed 27

--- codeTest27.cs
+++ codeTest27.cs
@@ -2,8 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-public class Solution {
-    public int RemoveElement(int[] nums, int val) {
-        
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums == null || nums.Length == 0) return 0;
+
+        int ret = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[ret] = nums[i];
+                ret++;
+            }
+        }
+
+        return ret;
     }
 }
