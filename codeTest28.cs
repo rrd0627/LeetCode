390bef13190e4c08d2c24fae40630d76c4bffadf
rrd0627 <rrd0627@gmail.com>
2021-10-24 01:25:43 +0900

    28

---
+++ codeTest28.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        
+        if(!haystack.Contains(needle))
+        {
+            Console.WriteLine("no!");
+            return -1;
+        }
+
+        int needleLen = needle.Length;
+
+        for(int i=0;i<haystack.Length - needleLen;i++)
+        {
+            if(haystack.Substring(i,needleLen)==needle)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
