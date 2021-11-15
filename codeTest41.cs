a9315c024997660e625340b6ebb99eb4cf5e3cc6
rrd0627 <rrd0627@gmail.com>
2021-11-15 13:00:25 +0900

    41

---
+++ codeTest41.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+public class Solution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        Array.Sort(nums);
+        int ret = 1;
+
+        HashSet<int> hash = new HashSet<int>();
+
+        int startIndex = 0;
+        for (startIndex = 0; startIndex < nums.Length; startIndex++)
+        {
+            if (nums[startIndex] > 0) break;
+        }
+
+        for (int i = startIndex; i < nums.Length; i++)
+        {
+            if (nums[i] == ret)
+            {
+                ret++;
+            }
+            else if(nums[i] > ret)
+            {
+                return ret;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        return ret;
+    }
+
+}
