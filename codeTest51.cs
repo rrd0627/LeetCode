27c28138d6c53cbc7105c83086f65bb83f7ed9c5
rrd0627 <rrd0627@gmail.com>
2021-11-27 21:51:24 +0900

    51

---
+++ codeTest51.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Solution {
+    public double MyPow(double x, int n) {
+        return Math.Pow(x,n);
+    }
+}
