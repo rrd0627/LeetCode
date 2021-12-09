56a76fe9a4da624a38b53ac64b72681e847d4ed5
rrd0627 <rrd0627@gmail.com>
2021-12-10 00:07:11 +0900

    60 fixed

--- codeTest60.cs
+++ codeTest60.cs
@@ -2,22 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+
 public class Solution
 {
     public string GetPermutation(int n, int k)
     {
-        List<int> nums = new List<int>(n);
-        for (int i = 0; i < n; i++)
-            nums.Add(i + 1);
+        List<int> nums = new List<int>();
+        nums.AddRange(Enumerable.Range(1, n));
 
         int nfact = 1;
         for (int i = 1; i <= n; i++)
             nfact *= i;
 
-        string ret = "";
+        StringBuilder ret = new StringBuilder();
 
         int blockSize = nfact;
-        int curIndex = k;
+        int curIndex = k - 1;
         int blockIndex = 0;
         while (nums.Count > 0)
         {
@@ -25,16 +25,16 @@ public class Solution
             blockSize = blockSize / n;
 
             //쪼갠 블럭중 몇번째 블럭인지
-            blockIndex = Math.Clamp((curIndex - 1) / blockSize, 0, blockSize);
+            blockIndex = curIndex / blockSize;
 
             //몇번째 블럭인지를 토대로 ret작성
-            ret += nums[blockIndex].ToString();
+            ret.Append(nums[blockIndex]);
             nums.RemoveAt(blockIndex);
 
             //다음을 위해서 
             n--;
-            curIndex = (curIndex - 1) % blockSize;
+            curIndex = curIndex % blockSize;
         }
-        return ret;
+        return ret.ToString();
     }
 }
