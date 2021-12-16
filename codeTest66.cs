c20d3a60e73a3701c34a2122b889d27fd30930b2
rrd0627 <rrd0627@gmail.com>
2021-12-16 23:26:43 +0900

    66

---
+++ codeTest66.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] != 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+                if (i == 0)
+                {
+                    int[] ret = new int[digits.Length + 1];
+                    ret[0] = 1;
+                    return ret;
+                }
+            }
+        }
+        return digits;
+    }
+}}
