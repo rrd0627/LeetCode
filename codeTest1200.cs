e5e7814b112043ae15a729ee40e39b3975f775aa
rrd0627 <rrd0627@gmail.com>
2021-12-20 23:58:08 +0900

    1200

--- codeTest1200.cs
+++ codeTest1200.cs
@@ -2,8 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-public class Solution {
-    public IList<IList<int>> MinimumAbsDifference(int[] arr) {
-        
+public class Solution
+{
+    public IList<IList<int>> MinimumAbsDifference(int[] arr)
+    {
+        Array.Sort(arr);
+
+        List<IList<int>> ret = new List<IList<int>>();
+
+        List<int> curList;
+
+        int minAbsDiff = int.MaxValue;
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int diff = arr[i] - arr[i - 1];
+
+            if (diff < minAbsDiff)
+            {
+                //새로 해야댐
+                minAbsDiff = diff;
+
+                ret.Clear();
+
+                curList = new List<int>() { arr[i - 1], arr[i] };
+
+                ret.Add(curList);
+            }
+            else if (diff == minAbsDiff)
+            {
+                //같은건 ret에 더해주기
+                curList = new List<int>() { arr[i - 1], arr[i] };
+
+                ret.Add(curList);
+            }
+        }
+        return ret;
     }
 }
