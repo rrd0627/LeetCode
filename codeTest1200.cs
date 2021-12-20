157b7b39213c1471584f2959f9250727e6468f93
rrd0627 <rrd0627@gmail.com>
2021-12-20 23:46:49 +0900

    1200

---
+++ codeTest1200.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+public class Solution {
+    public IList<IList<int>> MinimumAbsDifference(int[] arr) {
+        
+    }
+}
