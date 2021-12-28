dbe0bcbd74b2436c8e94e2cb73e58ffeea51606e
rrd0627 <rrd0627@gmail.com>
2021-12-29 00:00:37 +0900

    765

--- codeTest876.cs
+++ codeTest876.cs
@@ -2,19 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
-public class Solution {
-    public ListNode MiddleNode(ListNode head) {
-        
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        List<ListNode> listNodes = new List<ListNode>();
+
+        while (head != null)
+        {
+            listNodes.Add(head);
+            head = head.next;
+        }
+
+        return listNodes[listNodes.Count / 2];
     }
 }
