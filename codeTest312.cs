18432bcf56019ff237e29b1a7ef0bbb7fa665a1d
rrd0627 <rrd0627@gmail.com>
2022-01-01 23:53:59 +0900

    312

---
+++ codeTest312.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Solution
+{
+    int[][] dp;
+    int[] newNums;
+    public int MaxCoins(int[] nums)
+    {
+        newNums = new int[nums.Length + 2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            newNums[i + 1] = nums[i];
+        }
+        newNums[0] = newNums[newNums.Length - 1] = 1;
+        for (int i = 0; i < newNums.Length; i++)
+        {
+            System.Console.WriteLine(newNums[i]);
+        }
+        dp = new int[newNums.Length][];
+        for (int i = 0; i < dp.Length; i++)
+        {
+            dp[i] = new int[newNums.Length];
+        }
+
+        return func(0, newNums.Length - 1);
+    }
+    int func(int left, int right)
+    {
+        if (left + 1 == right) return 0;
+
+        if (dp[left][right] != 0) return dp[left][right];
+
+        int ret = 0;
+        for (int i = left + 1; i < right; i++)
+        {
+            ret = Math.Max(ret, newNums[left] * newNums[i] * newNums[right] + dp[left][i] + dp[i][right]);
+        }
+        dp[left][right] = ret;
+        return ret;
+    }
+}
