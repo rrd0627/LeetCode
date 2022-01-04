411363daa5738d51c2e9246cc8ca3bb5de101e78
rrd0627 <rrd0627@gmail.com>
2022-01-04 23:55:52 +0900

    doing 997

--- codeTest997.cs
+++ codeTest997.cs
@@ -5,30 +5,38 @@ using System.Linq;
 
 public class Solution
 {
-    public int NumPairsDivisibleBy60(int[] time)
+    bool comp(int[] a, int[] b)
     {
-        int[] arr = new int[60];
-
-        for (int i = 0; i < time.Length; i++)
+        if (a[0] <= b[0])
         {
-            arr[time[i] % 60]++;
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int FindJudge(int n, int[][] trust)
+    {
+        Array.Sort(trust);
+
+        HashSet<int> retSet = new HashSet<int>();
+
+        for (int i = 0; i < trust.Length; i++)
+        {
+            if (retSet.Contains(trust[i][0]))
+            {
+                retSet.Remove(trust[i][0]);
+            }
+            retSet.Add(trust[i]);
         }
 
-        int ret = 0;
+        if(retSet.Count==1)
+        {
+            return 1;
+        }
+        return -1;
 
-        for (int i = 1; i < 30; i++)
-        {
-            ret += arr[i] * arr[60 - i];
-        }
-
-        if (arr[0] >= 2)
-        {
-            ret += (arr[0] * (arr[0] - 1)) / 2;
-        }
-        if (arr[30] >= 2)
-        {
-            ret += (arr[30] * (arr[30] - 1)) / 2;
-        }
-        return ret;
     }
 }
