a065ea23d0eaeadec82bec4c4b5217cf347ab3e0
rrd0627 <rrd0627@gmail.com>
2022-01-05 23:27:03 +0900

    131

--- codeTest131.cs
+++ codeTest131.cs
@@ -19,27 +19,27 @@ public class Solution
 
     void dfs(int start, List<string> retTemp)
     {
-        System.Console.WriteLine(start);
-
         if (start >= _s.Length)
         {
             ret.Add(new List<string>(retTemp));
             return;
         }
+
         for (int end = start; end < _s.Length; end++)
         {
+
             //중간이 팰린드롬이면서 끝쪽이 같으면
-            if ((_s[start] == _s[end]) && (dp[start + 1, end - 1] || end - start <= 2))
+            if ((_s[start] == _s[end]) && (end - start <= 2 || dp[start + 1, end - 1]))
             {
                 dp[start, end] = true;
                 //retTemp를 넣어주고
-                retTemp.Add(_s.Substring(start, end - start));
+                retTemp.Add(_s.Substring(start, end - start + 1));
                 //다음것 돌려주고
                 dfs(end + 1, retTemp);
-                //다시 빼주어서 retTemp를 유지함!
-                retTemp.RemoveAt(retTemp.Count() - 1);
+                //원래자리로 돌아오기
+                retTemp.RemoveAt(retTemp.Count - 1);
             }
+
         }
     }
-
 }
