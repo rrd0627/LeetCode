40c41fa17044fdf7b2e14ec9a189d7433ca5ead1
rrd0627 <rrd0627@gmail.com>
2022-01-12 22:23:55 +0900

    701

---
+++ codeTest701.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+// Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root == null)
+            return new TreeNode(val);
+
+        if (root.val > val)
+        {
+            root.left = InsertIntoBST(root.left, val);
+        }
+        else
+        {
+            root.right = InsertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
